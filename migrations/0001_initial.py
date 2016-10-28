030e920786d1eee5e90aa0731c253873715c4443
Bob Leers <bobleers@fastmail.fm>
2016-10-28 23:28:48 +0200

    Inital data migration.

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255, unique=True, verbose_name='title')),
-                ('description', models.TextField(blank=True, help_text='For backend/admin usage only.', verbose_name='description')),
+                ('description', models.TextField(blank=True, help_text='For administration purposes only.', verbose_name='description')),
             ],
         ),
         migrations.CreateModel(
