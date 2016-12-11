bb0b2fa8345a9fe9463911ec2e23f3e281f1e46a
Bob Leers <bobleers@fastmail.fm>
2016-12-11 21:22:50 +0100

    Add thumb(image) on Product and Category modeladmin index. Fix form_valid for both with page check.

--- modeladmin.py
+++ modeladmin.py
@@ -1,10 +1,12 @@
 from django.shortcuts import redirect
 from django.templatetags.static import static
+from django.urls import reverse
+from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
 
 from treebeard.admin import TreeAdmin
 
-from wagtail.contrib.modeladmin.options import ModelAdmin, ModelAdminGroup
+from wagtail.contrib.modeladmin.options import ModelAdmin, ModelAdminGroup, ThumbnailMixin
 from wagtail.contrib.modeladmin.views import CreateView, EditView, IndexView
 from wagtail.wagtailadmin import messages
 from wagtail.wagtailcore.models import Page
@@ -55,10 +57,9 @@ class ProductEditView(EditView):
             parent_page.add_child(instance=product_page)
             instance.product_page = product_page
             instance.save()
-        else:
-            if form.data.get('image'):
-                instance.product_page.image = instance.image
-                instance.product_page.save()
+        elif instance.product_page and form.data.get('image'):
+            instance.product_page.image = instance.image
+            instance.product_page.save()
         
         messages.success(
             self.request, self.get_success_message(instance),
@@ -66,16 +67,46 @@ class ProductEditView(EditView):
         )
         return redirect(self.get_success_url())
 
-class ProductModelAdmin(ModelAdmin):
+class ProductModelAdmin(ThumbnailMixin, ModelAdmin):
     model = Product
+    
+    menu_icon = 'fa-product-hunt'
+    
     create_view_class = ProductCreateView
     edit_view_class = ProductEditView
-    menu_icon = 'fa-product-hunt'
 
     # TODO: add date_published (from ProductPage)
-    list_display = ['title', 'image', 'sale_price', 'product_page', 'sku', 'ean']
+    list_display = ['admin_thumb', 'title', 'sale_price', 'product_page_link', 'sku', 'ean']
+    list_display_add_buttons = 'title'
+    
     search_fields = ('title', 'product_page', 'sku', 'ean',)
     form_view_extra_css = [static('wagtailcommerce/css/core.css')]
+
+    thumb_image_field_name = 'image'
+    thumb_default = static('wagtailcommerce/img/no-image.png')
+
+    def product_page_link(self, obj):
+        if obj.product_page:
+            return format_html(
+                '{}\
+                <ul class="actions">\
+                <li><a class="button button-small button-secondary" href="{}">{}</a></li></ul>',
+                obj.product_page,
+                reverse('wagtailadmin_pages:edit', args=(obj.product_page.id,)),
+                'edit',
+                #obj.product_page
+            )
+        else:
+            return obj.product_page
+    product_page_link.short_description = _('product page')
+    
+    # def image_display(self, obj):
+    #     if obj.image:
+    #         rendition = get_rendition_or_not_found(obj.image, 'fill-50x50')
+    #         return rendition.img_tag()
+    #     else:
+    #         return obj.image
+    # image_display.short_description = _('image')
 
 """
 Category
@@ -127,8 +158,7 @@ class CategoryEditView(EditView):
             parent_page.add_child(instance=category_page)
             instance.category_page = category_page
             instance.save()
-        else:
-            if form.data.get('image'):
+        elif instance.category_page and form.data.get('image'):
                 instance.category_page.image = instance.image
                 instance.category_page.save()
         
@@ -148,30 +178,48 @@ class TreeModelAdmin(ModelAdmin, TreeAdmin):
     def get_node(self, node_id):
         return self.model.objects.get(pk=node_id)
     
-class CategoryModelAdmin(TreeModelAdmin):
+class CategoryModelAdmin(ThumbnailMixin, TreeModelAdmin):
     model = Category
+    
+    menu_icon = 'fa-tag'
+    
     index_view_class = CategoryIndexView
-    index_template_name = 'treebeard/admin/tree_change_list.html'
+    create_view_class = CategoryCreateView
+    edit_view_class = CategoryEditView
     
     index_view_extra_css = (
         'admin/css/base.css',
         'admin/css/changelists.css',
         'treebeard/treebeard-admin.css',
     )
-
     index_view_extra_js = (
         'treebeard/treebeard-admin.js'
     )
-    
-    create_view_class = CategoryCreateView
-    edit_view_class = CategoryEditView
-    menu_icon = 'fa-tag'
+    index_template_name = 'treebeard/admin/tree_change_list.html'
 
     # TODO: add date_published (from ProductPage)
-    list_display = ['title', 'image', 'category_page']
+    list_display = ['title', 'category_page_link', 'admin_thumb']
     search_fields = ('title', 'category_page')
+
     form_view_extra_css = [static('wagtailcommerce/css/core.css')]
 
+    thumb_image_field_name = 'image'
+    thumb_image_filter_spec = 'fill-30x30'
+    thumb_image_width = 30
+    thumb_default = static('wagtailcommerce/img/no-image.png')
+
+    def category_page_link(self, obj):
+        if obj.category_page:
+            return format_html(
+                '<a href="{}">{}</a>',
+                reverse('wagtailadmin_pages:edit', args=(obj.category_page.id,)),
+                obj.category_page,
+            )
+        else:
+            return obj.category_page
+    category_page_link.short_description = _('category page')
+    
+    
 """
 Commerce Main
 """
