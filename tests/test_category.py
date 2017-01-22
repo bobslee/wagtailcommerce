67662ba406d2e77e8f956afd864a6f005c5aa617
Bob Leers <bobleers@fastmail.fm>
2017-01-22 13:19:03 +0100

    Add several tests for category.

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,10 +1,10 @@
-from ..models import Category
-
 from .test_commerce import CommerceTestCase
+
+from ..models import Category
 
 class CategoryTestCase(CommerceTestCase):
 
-    def test_category_tree_all_active(self):
+    def test_tree_all_active(self):
 
         cooking_books_tree = Category.get_tree_active([self.cooking_books_category.id])
 
@@ -12,7 +12,7 @@ class CategoryTestCase(CommerceTestCase):
         self.assertIn(self.asian_cooking_books_category, cooking_books_tree)
         self.assertIn(self.sushi_asian_cooking_books_category, cooking_books_tree)
 
-    def test_category_tree_inactive_root(self):
+    def test_tree_inactive_root(self):
 
         self.cooking_books_category.active = False
         self.cooking_books_category.save()
@@ -21,7 +21,7 @@ class CategoryTestCase(CommerceTestCase):
 
         self.assertEqual(len(cooking_books_tree), 0)
 
-    def test_category_tree_inactive_subnode(self):
+    def test_tree_inactive_subnode(self):
 
         # Inactive node with children
         self.asian_cooking_books_category.active = False
@@ -35,3 +35,54 @@ class CategoryTestCase(CommerceTestCase):
 
         asian_cooking_books_tree = Category.get_tree_active([self.asian_cooking_books_category.id])
         self.assertEqual(len(asian_cooking_books_tree), 0)
+
+    def test_initial_parent_page(self):
+        category_page = self.asian_cooking_books_category.category_page
+        self.assertEqual(category_page.get_parent(), self.category_index_page)
+
+        category_page = self.sushi_asian_cooking_books_category.category_page
+        self.assertEqual(category_page.get_parent(), self.category_index_page)
+
+    def test_create_page(self):
+        self.assertIsNone(self.books_category.category_page)
+
+        self.books_category.create_page()
+        
+        self.assertEqual(self.books_category.category_page.title, self.books_category.title)
+        self.assertEqual(self.books_category.category_page.get_parent().specific, self.category_index_page)
+
+    def test_create_page_and_check_parent_page(self):
+        self.books_category.create_page()
+        self.books_category.save()
+
+        self.cooking_books_category.create_page()
+        self.assertEqual(self.cooking_books_category.category_page.title, self.cooking_books_category.title)
+        self.assertEqual(
+            self.cooking_books_category.category_page.get_parent().specific,
+            self.books_category.category_page)
+        
+        self.asian_cooking_books_category.create_page()
+        self.assertEqual(self.asian_cooking_books_category.category_page.title, self.asian_cooking_books_category.title)
+        self.assertEqual(
+            self.asian_cooking_books_category.category_page.get_parent().specific,
+            self.books_category.category_page)
+        
+    def test_create_page_and_update_parent_page_in_children(self):
+        """Create page for category, which has children with page already,
+        but under parent at ancestor level
+        """
+
+        # Current parent_page
+        asian_page = self.asian_cooking_books_category.category_page
+        self.assertEqual(asian_page.get_parent().specific, self.category_index_page)
+
+        # Create page for parent_page
+        self.cooking_books_category.create_page()
+        self.cooking_books_category.save()
+        self.assertEqual(self.cooking_books_category.category_page.title, self.cooking_books_category.title)
+
+        # parent_page should be changed
+        self.assertEqual(asian_page.get_parent().specific, self.cooking_books_category.category_page)
+
+    # def test_category_tree_with_live_page(self):
+    #     asian_cooking_books_tree = Category.get_tree_active([self.asian_cooking_books_category.id])
