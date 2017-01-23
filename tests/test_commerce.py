42b05feafc1737f1fda4a751385d0b2715ffaf6a
Bob Leers <bobleers@fastmail.fm>
2017-01-23 22:39:02 +0100

    Add products in setUp() of CommerceTestCase.

--- tests/test_commerce.py
+++ tests/test_commerce.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
 
-from ..models import Category, CategoryIndexPage, CategoryPage
+from ..models import Category, CategoryIndexPage, CategoryPage, Product
 
 class CommerceTestCase(TestCase):
     def setUp(self):
+        super(CommerceTestCase, self).setUp()
         self.setup_categories()
+        self.setup_products()
 
     def setup_categories(self):
 
@@ -79,3 +81,79 @@ class CommerceTestCase(TestCase):
 
         viking_ancient_history_books = Category(title="Viking Ancient History Books", active=True)
         self.viking_ancient_history_books_category = self.ancient_history_books_category.add_child(instance=viking_ancient_history_books)
+
+    def setup_products(self):
+
+        """sushi_chef_a_cooking_book_product"""
+        self.sushi_chef_a_cooking_book_product = Product(
+            title="Sushi Chef A Cooking Book",
+            sale_price=10.00,
+            cost_price=7.00
+        )
+        self.sushi_chef_a_cooking_book_product.save()
+        self.sushi_chef_a_cooking_book_product.categories.add(self.asian_cooking_books_category, self.sushi_asian_cooking_books_category)
+
+        """sushi_chef_b_cooking_book_product"""
+        self.sushi_chef_b_cooking_book_product = Product(
+            title="Sushi Chef B Cooking Book",
+            sale_price=22.00,
+            cost_price=15.00
+        )
+        self.sushi_chef_b_cooking_book_product.save()
+        self.sushi_chef_b_cooking_book_product.categories.add(self.sushi_asian_cooking_books_category)
+
+        """wok_chef_a_cooking_book_product"""
+        self.wok_chef_a_cooking_book_product = Product(
+            title="Wok Chef A Cooking Book",
+            sale_price=12.50,
+            cost_price=6.50
+        )
+
+        # TODO check 2 categories to search/query?
+        self.wok_chef_a_cooking_book_product.save()
+        self.wok_chef_a_cooking_book_product.categories.add(self.asian_cooking_books_category, self.wok_asian_cooking_books_category)
+
+        """wok_chef_b_cooking_book_product"""
+        self.wok_chef_b_cooking_book_product = Product(
+            title="Wok Chef B Cooking Book",
+            sale_price=32.00,
+            cost_price=15.00
+        )
+        self.wok_chef_b_cooking_book_product.save()
+        self.wok_chef_b_cooking_book_product.categories.add(self.wok_asian_cooking_books_category)
+
+        """italian_a_cooking_book_product"""
+        self.italian_a_cooking_book_product = Product(
+            title="Italian A Cooking Book",
+            sale_price=31.00,
+            cost_price=19.00
+        )
+        self.italian_a_cooking_book_product.save()
+        self.italian_a_cooking_book_product.categories.add(self.italian_cooking_books_category)
+
+        """pasta_a_italian_cooking_book_product"""
+        self.pasta_a_italian_cooking_book_product = Product(
+            title="Pasta A Italian Cooking Book",
+            sale_price=15.50,
+            cost_price=10.00
+        )
+        self.pasta_a_italian_cooking_book_product.save()
+        self.pasta_a_italian_cooking_book_product.categories.add(self.pasta_italian_cooking_books_category)
+
+        """pasta_b_italian_cooking_book_product"""
+        self.pasta_b_italian_cooking_book_product = Product(
+            title="Pasta B Italian Cooking Book",
+            sale_price=20.95,
+            cost_price=17.95
+        )
+        self.pasta_b_italian_cooking_book_product.save()
+        self.pasta_b_italian_cooking_book_product.categories.add(self.pasta_italian_cooking_books_category)
+
+        """pasta_c_italian_cooking_book_product"""
+        self.pasta_c_italian_cooking_book_product = Product(
+            title="Pasta C Italian Cooking Book",
+            sale_price=8.20,
+            cost_price=4.20
+        )
+        self.pasta_c_italian_cooking_book_product.save()
+        self.pasta_c_italian_cooking_book_product.categories.add(self.pasta_italian_cooking_books_category)
